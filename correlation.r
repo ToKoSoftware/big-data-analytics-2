aa2c8680417a3fdee7979b1560aa544bfb3fa22e
Dominik Ruf <aquanik.ruf@gmail.com>
2022-02-23 11:08:09 +0100

    Update correlation

--- correlation.r
+++ correlation.r
@@ -1,10 +1,9 @@
+install.packages("foreign")
+library(foreign)
+
 dat=read.dta("CARD.DTA")
 min(dat$age)
 median(dat$age)
-
-model = summary(lm(dat$age~dat$wage))
-model
-plot(dat$age, dat$wage)
 
 # Die Effektstärke ist im Rahmen der Korrelation der Korrelationskoeffizient r selbst. Laut Cohen: Statistical Power Analysis for the Behavioral Sciences (1988), S. 79-81 sind die Effektgrenzen
 #ab 0,1 (schwach),
@@ -12,12 +11,11 @@ plot(dat$age, dat$wage)
 #ab 0,5 (stark).
 # [0-0.1], [0.1, 0,3], [0.3, 0.5], [0.5, 1]
 
-subset_cor = subset(dat, select = c(nearc2, nearc4, educ, age, fatheduc, motheduc, black, wage, IQ, exper), na.rm = TRUE)
+subset_cor = subset(dat, select = c(nearc2, nearc4, educ, age, fatheduc, motheduc, momdad14, sinmom14, step14, south66, smsa, south, smsa66, enroll, KWW, married, libcrd14, black, wage, IQ, exper, lwage, expersq), na.rm = TRUE)
 korr_tab = cor(subset_cor)
 korr_tab
 write.csv(korr_tab, file = "Korrelationstabelle.csv", sep = ";")
 
-min(dat$KWW)
 
 # Schwarze haben einen geringeren Stundenlohn als Weiße (black, wage)
 wage_black = summary(lm(dat$wage~dat$black))
